095287b3374fe82ae75516f99af56ff62ec1c23e
lostinstarvation <93753846+lostinstarvation@users.noreply.github.com>
2021-11-06 18:07:50 -0700

    Add files via upload

--- SunriseClock/sunriseAnimation.ino
+++ SunriseClock/sunriseAnimation.ino
@@ -3,31 +3,31 @@ void sunriseAnimation(){
  static int8_t checkVar = 0;
   
   if(rtc.hour() == 6 && rtc.minute() == 0 && checkVar == 0){
-      FastLED.setBrightness(5);      
+      FastLED.setBrightness(8);      
       for (int i = 0; i < NUM_LEDS; i++) {
-        leds[i].red = 173;
-        leds[i].blue = 255;
+        leds[i].red = 130;
+        leds[i].blue = 199;
         leds[i].green = 218;
         FastLED.show();
       }   
     checkVar = 1;
   }
   if(rtc.hour() == 6 && rtc.minute() == 8 && checkVar == 1){
-    FastLED.setBrightness(10);
+    FastLED.setBrightness(14);
     FastLED.show();
     checkVar = 2;
   }
-  if(rtc.hour() == 8 && rtc.minute() == 16 && checkVar == 2){
-    FastLED.setBrightness(15);
+  if(rtc.hour() == 6 && rtc.minute() == 16 && checkVar == 2){
+    FastLED.setBrightness(20);
     FastLED.show();
     checkVar = 3;
   }
-  if(rtc.hour() == 8 && rtc.minute() == 24 && checkVar == 3){
-    FastLED.setBrightness(30);
+  if(rtc.hour() == 6 && rtc.minute() == 24 && checkVar == 3){
+    FastLED.setBrightness(32);
     FastLED.show();
     checkVar = 4;
   }
-  if(rtc.hour() == 8 && rtc.minute() == 35 && checkVar == 4){
+  if(rtc.hour() == 6 && rtc.minute() == 35 && checkVar == 4){
     for (int i = 0; i < NUM_LEDS; i++) {
       leds[i].red = 0;
       leds[i].blue = 0;
