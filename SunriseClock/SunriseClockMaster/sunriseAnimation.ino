aee550322159c6cfac4321763a4e29565307c08a
lostinstarvation <93753846+lostinstarvation@users.noreply.github.com>
2022-02-10 19:49:08 -0800

    Update sunriseAnimation.ino

--- SunriseClock/SunriseClockMaster/sunriseAnimation.ino
+++ SunriseClock/SunriseClockMaster/sunriseAnimation.ino
@@ -3,7 +3,7 @@ void sunriseAnimation(){
  static int8_t checkVar = 0;
   
   if(rtc.hour() == 6 && rtc.minute() == 0 && checkVar == 0){
-      FastLED.setBrightness(8);      
+      FastLED.setBrightness(10);      
       for (int i = 0; i < NUM_LEDS; i++) {
         leds[i].red = 130;
         leds[i].blue = 199;
@@ -13,22 +13,22 @@ void sunriseAnimation(){
     checkVar = 1;
   }
   if(rtc.hour() == 6 && rtc.minute() == 6 && checkVar == 1){
-    FastLED.setBrightness(14);
+    FastLED.setBrightness(20);
     FastLED.show();
     checkVar = 2;
   }
   if(rtc.hour() == 6 && rtc.minute() == 12 && checkVar == 2){
-    FastLED.setBrightness(20);
+    FastLED.setBrightness(26);
     FastLED.show();
     checkVar = 3;
   }
   if(rtc.hour() == 6 && rtc.minute() == 18 && checkVar == 3){
-    FastLED.setBrightness(26);
+    FastLED.setBrightness(32);
     FastLED.show();
     checkVar = 4;
   }
   if(rtc.hour() == 6 && rtc.minute() == 24 && checkVar == 4){
-    FastLED.setBrightness(32);
+    FastLED.setBrightness(40);
     FastLED.show();
     checkVar = 5;
   }
